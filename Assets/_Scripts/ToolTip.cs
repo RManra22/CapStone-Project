5e63ffdbf3e2f0c33f16bbb30e19010e4679f650
iwant22 <robertomanra22@gmail.com>
2026-04-08 22:23:09 -0700

    Added a some more buttons and a little script that shows text of what the button will do.

---
+++ Assets/_Scripts/ToolTip.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject tooltipObject;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tooltipObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltipObject.SetActive(false);
+    }
+}
