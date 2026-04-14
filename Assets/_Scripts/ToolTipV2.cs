e587d0526b86ff0272cc13df5b92b093a94ae960
iwant22 <robertomanra22@gmail.com>
2026-04-13 20:13:23 -0700

    Added animated background to all menus I think, and added gamemode explanation windows in gamemode select scene.

---
+++ Assets/_Scripts/ToolTipV2.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class TooltipV2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject tooltipObject;
+    public GameObject tooltipObject2;
+    public GameObject tooltipObject3;
+    public GameObject tooltipObject4;
+    
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tooltipObject.SetActive(true);
+        tooltipObject2.SetActive(true);
+        tooltipObject3.SetActive(true);
+        tooltipObject4.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltipObject.SetActive(false);
+        tooltipObject2.SetActive(false);
+        tooltipObject3.SetActive(false);
+        tooltipObject4.SetActive(false);
+    }
+}
