5f7b96c9e8129c66142141cb7b4153b454602429
Alex Zhou <179976662+notuohz@users.noreply.github.com>
2026-04-14 21:10:42 -0700

    Added loading scene

--- Assets/_Scripts/ButtonManager.cs
+++ Assets/_Scripts/ButtonManager.cs
@@ -1,14 +1,28 @@
-using JetBrains.Annotations;
-using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour
 {
+
+    public GameObject loadingUI; // Reference to the loading UI GameObject
     // This script is responsible for allowing the player to navigate the menus as per the functional requirements. 
     public void LoadScene(string sceneName) // load the correct scene based on the name given
     {
 
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        loadingUI.SetActive(true); // Show the loading UI
+        StartCoroutine(LoadAsync(sceneName));
     }
+    IEnumerator LoadAsync(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+
     // This script is responsible for allowing the player to exit the game as per the functional requirements.
     public void ExitGame() // quit the application from any menu
     {
